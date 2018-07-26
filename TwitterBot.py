21699896b2b858804a1734d37bcee0f7fc29805a
Sanyam Dhawan <sanyamdhawan0001@gmail.com>
2018-07-26 19:11:14 +0530

    Update TwitterBot.py

--- TwitterBot.py
+++ TwitterBot.py
@@ -1,10 +1,10 @@
 import tweepy
 
 # used titter app to get these values
-consumer_key = 'enyvfQfcNJStRmKwvtIguuy5A'
-consumer_secret = 'PRaQdiwPYHYavP2gWy8UaIFDbSAVKXSJB2wYt46TpkqihRuhiX'
-access_token = '900680404536930306-m4eaEHTTtARJ4l1ySAUJcLvJxe1YW4m'
-access_token_secret = 'HO2MRrije8VgUMeey0H4TxNgXbpdSb11XAXWm2o4II1ZI'
+consumer_key = #enter key
+consumer_secret = #enter secret
+access_token = #enter access token
+access_token_secret = #enter access token secret
 
 # authorization using OAuthHandler
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -20,7 +20,7 @@ print(user.name) # will print the name of the user
 
 def main():
     # set up the search criteria
-    search = ("coding")
+    search = ("coding")  # enter any keyword
     number_of_tweets = 100
     """
         api.search --> search in the api and returns items number of tweets time
